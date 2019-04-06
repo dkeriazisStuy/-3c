6263099d9e93ca7d49d38d482231bfdacd3cd5b2
JackLu1 <jlu2025@gmail.com>
2019-04-06 18:20:22 -0400

    WIP

--- app/static/filter.js
+++ app/static/filter.js
@@ -9,17 +9,18 @@ d3.csv("./static/Big_Cities_Health_Data_Inventory.csv").then(function(data){
         var selection = body.append('select')
             .attr('class', 'select')
             .attr('name', keys[i])
-            .on('change', function(){
-                // get all data values corresponding to key 
-                selectValue = selection.property('value')
-                category = selection.attr('name');
+            // .on('change', function(){
+            //     // get all data values corresponding to key 
+            //     selectValue = selection.property('value')
+            //     category = selection.attr('name');
 
-                console.log('===filter===');
-                var info = filter(data, category, selectValue);
-                console.log(info);
-                console.log('===filter===');
-            });
-        body.append('br')
+            //     console.log('===filter===');
+            //     var info = filter(data, category, selectValue);
+            //     console.log(info);
+            //     console.log('===filter===');
+            // });
+        body.append('br');
+        body.append('br');
         // get unique values for each key
         var options = selection.selectAll('option');
         options.append('option')
@@ -28,88 +29,16 @@ d3.csv("./static/Big_Cities_Health_Data_Inventory.csv").then(function(data){
         options.data(getChoices(data, keys[i])).enter()
             .append('option')
             .text(function(d){ return d; })
-    }
-
-    // var selections = body.selectAll('select')
-    // console.log(selections);
-    //    .data(keys).enter()
-    //    .append('text').text(function(k){console.log(k); return k;})
-    //    .append('select')
-    //        .attr('class', 'select')
-    //        .on('change', function(){
-    //            // get all data values corresponding to key 
-    //            selectValue = d3.select('select').property('value')
-    //            console.log('===filter===');
-    //            var info = filter(data, category, selectValue);
-    //            console.log(info);
-    //            console.log('===filter===');
-    //        });
-    //var a = body.selectAll('text')
-    //    
-    //console.log(a);
-
-
-    //console.log(options);
-
-
-    /*
-    keys.forEach(function(category){ 
-    //console.log(category);
-        var unique_vals = [];
-        for (var i = 0; i < data.length; i++){
-            if (!unique_vals.includes(data[i][category])){
-                unique_vals.push(data[i][category]);
-            }
-        }
-    //console.log(unique_vals);
-
-        var select = body
-            .text(category + ' ')
-            .append('select')
-                .attr('class', 'select')
-            .on('change', change)
-
-        var options = select.selectAll('option')
-            .data(unique_vals).enter()
-            .append('option')
-                .text( function(d) {return d;} );
-
-        console.log(options);
-
-        function change(){
-            selectValue = d3.select('select').property('value')
-            var info = filter(data, category, selectValue);
-            console.log(info);
-        };
-
-    });
-    */
-
-    //var places = [];
-    //for (var i = 0; i < data.length; i++){
-    //    if (!places.includes(data[i]["Place"])){
-    //        places.push(data[i]["Place"]);
-    //    }
-    //}
-
-    //var select = d3.select('body')
-    //    .text('Place ')
-    //    .append('select')
-    //        .attr('class', 'select')
-    //    .on('change', change)
-
-    //var options = select.selectAll('option')
-    //    .data(places).enter()
-    //    .append('option')
-    //        .text( function(d) {return d;} );
-
-    //function change(){
-    //    selectValue = d3.select('select').property('value')
-    //    var info = filter(data, 'Place', selectValue);
-    //    console.log(info);
-    //};
-
+    };
+    body.append('br');
+    body.append('button')
+        .attr('type', 'button')
+        .text('wee')
 });
+
+var getFilters = function(){
+
+}
 
 // return list of different choices in a category
 var getChoices = function(data, choice){
