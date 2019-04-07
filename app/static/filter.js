6f6b5ec5c8b0296d8b213dab12a0876f2d9214d2
JackLu1 <jlu2025@gmail.com>
2019-04-06 23:52:11 -0400

    finished filtering

--- app/static/filter.js
+++ app/static/filter.js
@@ -4,6 +4,7 @@ var keys = ["Year", "Gender", "Race/ Ethnicity", "Place"];
 
 d3.csv("./static/Big_Cities_Health_Data_Inventory.csv").then(function(data){
 
+    // 4 filters
     for (var i = 0; i < keys.length; i++){
         row = table.append('tr');
         row.append('td').text(keys[i]);
@@ -16,21 +17,34 @@ d3.csv("./static/Big_Cities_Health_Data_Inventory.csv").then(function(data){
             .text(function(d){ return d; })
     };
 
+    // submit button
     body.append('br');
     var b = body.append('button')
         .attr('type', 'button')
         .attr('id', 'button')
-        .attr('onclick', 'getFilters()')
-        .text('Submit');
+        .text('Submit')
+        .on('click', function(){
+            return getData(data);
+        });
 });
 
 
-var getFilters = function(){
+// get filtered data
+var getData = function(data){
     var filters = table.selectAll('select');
-    console.log(filters);
+    var values = [];
+    var results = data.slice(0);
+
     filters.each(function(d){
-        console.log(this.value);
+        values.push(this.value);
     });
+
+    for (var i = 0; i < keys.length; i++){
+        if (values[i] !== 'All' && values[i] !== 'Both'){
+            results = filter(results, keys[i], values[i]);
+        }
+    }
+    return results;
 };
 
 // return list of different choices in a category
@@ -48,16 +62,12 @@ var getChoices = function(data, choice){
 };
 
 // return all elements with value in category
-// var filter = function(data, category, value){
-//     var clean = [];
-//     for (var i = 0; i < data.length; i++){
-//         if (data[i][category] === value){
-//             clean.push(data[i]);
-//         }
-//     }
-//     //console.log('--------------------------------------------------');
-//     //console.log(category);
-//     //console.log(value);
-//     //console.log('--------------------------------------------------');
-//     return clean;
-// };
+var filter = function(data, category, value){
+    var clean = [];
+    for (var i = 0; i < data.length; i++){
+        if (data[i][category] === value){
+            clean.push(data[i]);
+        }
+    }
+    return clean;
+};
