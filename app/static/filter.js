fcdd8bdc085cc30abeec18015f48f647d17e2ae9
JackLu1 <jlu2025@gmail.com>
2019-04-06 20:00:05 -0400

    WIP

--- app/static/filter.js
+++ app/static/filter.js
@@ -1,10 +1,8 @@
-var keys = ["Year", "Gender", "Race/ Ethnicity", "Place"];
 var body = d3.select('#filter');
 var table = body.append("table");
+var keys = ["Year", "Gender", "Race/ Ethnicity", "Place"];
 
 d3.csv("./static/Big_Cities_Health_Data_Inventory.csv").then(function(data){
-    //console.log( Object.keys(data[0]) );
-
 
     for (var i = 0; i < keys.length; i++){
         row = table.append('tr');
@@ -12,23 +10,7 @@ d3.csv("./static/Big_Cities_Health_Data_Inventory.csv").then(function(data){
         var selection = row.append('td').append('select')
             .attr('class', 'select')
             .attr('name', keys[i])
-        // .on('change', function(){
-        //     // get all data values corresponding to key 
-        //     selectValue = selection.property('value')
-        //     category = selection.attr('name');
-
-        //     console.log('===filter===');
-        //     var info = filter(data, category, selectValue);
-        //     console.log(info);
-        //     console.log('===filter===');
-        // });
-        //body.append('br');
-        //body.append('br');
-        // get unique values for each key
         var options = selection.selectAll('option');
-        options.append('option')
-            .attr('selected', 'selected')
-            .text('test');
         options.data(getChoices(data, keys[i])).enter()
             .append('option')
             .text(function(d){ return d; })
@@ -59,22 +41,23 @@ var getChoices = function(data, choice){
             unique_vals.push(data[i][choice]);
         }
     }
-
+    if (choice === "Year" || choice === "Place"){
+        unique_vals.unshift("All");
+    }
     return unique_vals;
 };
 
 // return all elements with value in category
-var filter = function(data, category, value){
-    var clean = [];
-    for (var i = 0; i < data.length; i++){
-        if (data[i][category] === value){
-            clean.push(data[i]);
-        }
-    }
-    //console.log('--------------------------------------------------');
-    //console.log(category);
-    //console.log(value);
-    //console.log('--------------------------------------------------');
-    return clean;
-};
-
+// var filter = function(data, category, value){
+//     var clean = [];
+//     for (var i = 0; i < data.length; i++){
+//         if (data[i][category] === value){
+//             clean.push(data[i]);
+//         }
+//     }
+//     //console.log('--------------------------------------------------');
+//     //console.log(category);
+//     //console.log(value);
+//     //console.log('--------------------------------------------------');
+//     return clean;
+// };
