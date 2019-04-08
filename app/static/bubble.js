9b8f09c5e206be807d75c3e7e3855fe01969bce4
huiminwu <huiminwu24@gmail.com>
2019-04-08 00:54:03 -0400

    Bubble charts kinda work

--- app/static/bubble.js
+++ app/static/bubble.js
@@ -11,23 +11,32 @@ d3.csv("./static/Big_Cities_Health_Data_Inventory.csv").then(function(data) {
                 .attr('id', 'button')
                 .text('Submit')
                 .on('click', function() {
-                    raw = getData(data).slice(0);
-                    console.log('??');
-                    //console.log(raw);
-                });
+                    dset = getData(data).slice(0);
+                    //console.log('??');
+                    console.log(dset);
+                raw = {
+                    "children": dset
+                };
+                    var svg = d3.select("svg");
+                everything();
+    });
+    var test = {
+        "children": [{"Name":"HIV","Count":0.0304},
+            {"Name":"AIDS","Count":0.0785},
+            {"Name":"Cancer","Count":0.1958},
+            {"Name":"Injury","Count":0.0088},
+            {"Name":"Tuberculosis","Count":0.0035},
+            {"Name":"Heart Disease","Count":0.156},
+            {"Name":"Diabetes","Count":0.0143},
+            {"Name":"Pneumonia","Count":0.0119},
+            {"Name":"Firearm Related","Count":0.0137},
+            {"Name":"Homicide","Count":0.0116}]
+        };
+    console.log(test);
+});
 
-    dataset = {
-    "children": [{"Name":"HIV","Count":0.0304},
-        {"Name":"AIDS","Count":0.0785},
-        {"Name":"Cancer","Count":0.1958},
-        {"Name":"Injury","Count":0.0088},
-        {"Name":"Tuberculosis","Count":0.0035},
-        {"Name":"Heart Disease","Count":0.156},
-        {"Name":"Diabetes","Count":0.0143},
-        {"Name":"Pneumonia","Count":0.0119},
-        {"Name":"Firearm Related","Count":0.0137},
-        {"Name":"Homicide","Count":0.0116}]
-    };
+var everything = function(d) {
+
     var toolTip = d3.select('#bubble')
                     .append("tip")
                     .style("opacity", 0)
@@ -39,9 +48,10 @@ d3.csv("./static/Big_Cities_Health_Data_Inventory.csv").then(function(data) {
                     .style("color", "white")
 
     var showTooltip = function(d) {
+        console.log(d)
         toolTip.transition().duration(200)
         toolTip.style("opacity", 1)
-                .html("Disease: " + d.data.Name + "<br/>" + "Count: " + d.data.Count)
+                .html("Indicator: " + d.data.Indicator + "<br/>" + "Value: " + d.data.Value)
                 .style("left", d3.event.pageX + 20 + "px")
                 .style("top", d3.event.pageY + 20 + "px")
     }
@@ -60,7 +70,7 @@ d3.csv("./static/Big_Cities_Health_Data_Inventory.csv").then(function(data) {
     var diameter = 600;
     var color = d3.scaleOrdinal(d3.schemeCategory10);
 
-    var bubble = d3.pack(dataset)
+    var bubble = d3.pack(raw)
         .size([diameter, diameter])
         .padding(1.5);
 
@@ -70,8 +80,9 @@ d3.csv("./static/Big_Cities_Health_Data_Inventory.csv").then(function(data) {
         .attr("height", diameter)
         .attr("class", "bubble");
 
-    var nodes = d3.hierarchy(dataset)
-        .sum(function(d) { return d.Count; });
+    var nodes = d3.hierarchy(raw)
+        .sum(function(d) { 
+            return d.Value; });
 
     var node = svg.selectAll(".node")
         .data(bubble(nodes).descendants())
@@ -100,7 +111,7 @@ d3.csv("./static/Big_Cities_Health_Data_Inventory.csv").then(function(data) {
         .attr("stroke", "black")
         .attr("stroke-width", 0)
         .on('click', function(d) {
-            console.log(d.data.Name);
+            console.log(d.data.Indicator);
         })
         .on('mouseover', function(d) {
             d3.select(this)
@@ -125,7 +136,7 @@ d3.csv("./static/Big_Cities_Health_Data_Inventory.csv").then(function(data) {
         .attr("dy", ".2em")
         .style("text-anchor", "middle")
         .text(function(d) {
-            return d.data.Name.substring(0, d.r / 3);
+            return d.data.Indicator;//.substring(0, d.r / 3);
         })
         .attr("font-family", "sans-serif")
         .attr("font-size", function(d){
@@ -137,7 +148,7 @@ d3.csv("./static/Big_Cities_Health_Data_Inventory.csv").then(function(data) {
         .attr("dy", "1.3em")
         .style("text-anchor", "middle")
         .text(function(d) {
-            return d.data.Count;
+            return d.data.Value;
         })
         .attr("font-family",  "Gill Sans", "Gill Sans MT")
         .attr("font-size", function(d){
@@ -147,5 +158,5 @@ d3.csv("./static/Big_Cities_Health_Data_Inventory.csv").then(function(data) {
 
     d3.select(self.frameElement)
         .style("height", diameter + "px");
-});
+};
 
